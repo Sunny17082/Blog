9e878c7442d867362e38fc9a843c1dee3a165d57
Sunny17082 <sunnyprasad1708@gmail.com>
2023-12-28 23:07:56 +0530

    Added login and register

--- src/components/Pages/RegisterPage.jsx
+++ src/components/Pages/RegisterPage.jsx
@@ -5,14 +5,19 @@ function RegisterPage() {
     const [password, setPassword] = useState('');
     async function register(ev) {
         ev.preventDefault();
-        await fetch("http://localhost:5000/register", {
+        const response = await fetch("http://localhost:5000/register", {
             method: "POST",
             body: JSON.stringify({username, password}),
             headers: {'Content-Type':'application/json'}
         });
+        if(response.status === 200) {
+            alert("registration successful!");
+        } else {
+            alert("registration failed!")
+        }
     }
     return (
-        <form className="login" onSubmit={register}>
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
             <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}/>
             <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}/>
